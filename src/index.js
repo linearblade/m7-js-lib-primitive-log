b98464627b444c69768b16c89e843282cdceb0d6
linearblade <sin_vraal@hotmail.com>
2026-01-19 15:10:33 +0800

    added an index.js barrel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+// index.js
 import Manager from "./Manager.js";
 import Worker from "./Worker.js";
 import utils from "./utils.js";
 import * as constants from "./constants.js";
 
+// Handy named constant export (common import pattern)
+export const { CONSOLE_LEVEL } = constants;
 
-const log = {
-  Manager,
-  Worker,
-  utils,
-  constants
-};
+// Named exports
+export { Manager, Worker, utils, constants };
 
-export { log, Manager, Worker, utils, constants };
+// Default / namespace-style export
+const log = { Manager, Worker, utils, constants, CONSOLE_LEVEL };
+export { log };
 export default log;
